27ba2f0ed50459865fbc526eab21b8c83f5f9709
Anton <antonwelliver27@gmail.com>
2020-11-13 17:10:31 +0100

    admin order list

--- frontend/src/reducers/orderReducers.js
+++ frontend/src/reducers/orderReducers.js
@@ -1,4 +1,4 @@
-import { ORDER_CREATE_REQUEST, ORDER_CREATE_SUCCESS, ORDER_CREATE_FAIL, ORDER_DETAILS_REQUEST, ORDER_DETAILS_SUCCESS, ORDER_DETAILS_FAIL, ORDER_PAY_REQUEST, ORDER_PAY_SUCCESS, ORDER_PAY_FAIL, ORDER_PAY_RESET, ORDER_LIST_REQUEST, ORDER_LIST_SUCCESS, ORDER_LIST_FAIL, ORDER_LIST_RESET } from '../constants/orderConstants'
+import { ORDER_CREATE_REQUEST, ORDER_CREATE_SUCCESS, ORDER_CREATE_FAIL, ORDER_DETAILS_REQUEST, ORDER_DETAILS_SUCCESS, ORDER_DETAILS_FAIL, ORDER_PAY_REQUEST, ORDER_PAY_SUCCESS, ORDER_PAY_FAIL, ORDER_PAY_RESET, ORDER_LIST_REQUEST, ORDER_LIST_SUCCESS, ORDER_LIST_FAIL, ORDER_LIST_RESET, FULL_ORDER_LIST_REQUEST, FULL_ORDER_LIST_SUCCESS, FULL_ORDER_LIST_FAIL } from '../constants/orderConstants'
 
 export const orderCreateReducer = (state = {}, action) => {
     switch (action.type) {
@@ -85,6 +85,27 @@ export const orderListReducer = (state = { orders: [] }, action) => {
             }
         case ORDER_LIST_RESET:
             return { orders: [] }
+        default:
+            return state
+    }
+}
+
+export const fullOrderListReducer = (state = { orders: [] }, action) => {
+    switch (action.type) {
+        case FULL_ORDER_LIST_REQUEST:
+            return {
+                loading: true
+            }
+        case FULL_ORDER_LIST_SUCCESS:
+            return {
+                loading: false,
+                orders: action.payload
+            }
+        case FULL_ORDER_LIST_FAIL:
+            return {
+                loading: false,
+                error: action.payload
+            }
         default:
             return state
     }
